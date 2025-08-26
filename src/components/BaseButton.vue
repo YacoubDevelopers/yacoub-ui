8be4e80c51470c3dd7b31364b853b90d2df9e96b
0fra <fmaron@alu.frlp.utn.edu.ar>
2025-08-26 14:33:23 -0300

    init

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="rounded-lg bg-primary px-4 py-2 text-white hover:bg-primary/80 disabled:opacity-50"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
